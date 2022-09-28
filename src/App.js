ad61a24652b3c45365e274e5fb92beb8cf903797
GuruRohith <98203158+Rohith255@users.noreply.github.com>
2022-09-29 00:29:03 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import react,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
@@ -6,12 +6,17 @@ import Hero from './Components/Hero'
 import Menubar from './Components/Menubar';
 import Task from './Components/Task'
 function App() {
+  const [active, setActive] = useState(true);
+  function toggleActiveState()
+  {
+    setActive(!active);
+  }
+
   return (
     <div className="App">
-      <Navbar/>
-      <Hero/>
+      <Navbar  toggleActiveState={toggleActiveState}/>
+      <Hero active={active}/>
     </div>
   );
 }
-
 export default App;
